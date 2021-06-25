f664e43c557092e16f38f73156590e244940059e
DavidCT <davidcuervotunon@gmail.com>
2021-06-25 08:08:05 +0200

    Prueba 25.06.2021
    Prueba 25.06.2021

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-
-function obtenerDatosCliente() {
-    
+function generarControlesDinamicos(){
+    var nodoPadre = document.getElementById("controlesDinamicos");
+    var element3 = document.createElement("input");
+    element3.type = "button";
+    element3.name = "btnPrueba";
+    element3.value="Prueba Documentación";
+    element3.className="btn btn-primary";
+    nodoPadre.appendChild(element3);
 };
-
 
 
 
